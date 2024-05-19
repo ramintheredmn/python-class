7075711bb71ad98c0e71577990dbc5b5ac50cbe2
ramintheredmn <redcrossprog@gmail.com>
2024-05-19 23:11:42 +0330

    first commit

---
+++ 1stquiz.py
@@ -0,0 +1,27 @@
+# storing the password in a variable named password with init value password
+password = "class"
+
+# create a variable named userInput with init value " ", 0
+userInput = ""
+
+# create variable named Edame with init value
+Edame = True
+
+
+# create the sentry variable
+
+i = 0
+
+# while loop for asking the user guess and store it in variable userInput
+
+while Edame:
+    userInput = input("Kalamaye obor ra vared konid? : ")
+    if userInput == password and i < 3:
+        print("success")
+        Edame = False
+    elif userInput != password and i < 2 :
+        print("Again")
+        i = i + 1
+    elif userInput != password and i ==2 :
+        print("your out of guesses ! goodbye ")
+        Edame = False
